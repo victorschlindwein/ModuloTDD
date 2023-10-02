6812bc7831f98f52883b5041cef827e56e9f52f8
dizzysch <victorwilbert@gmail.com>
2023-10-02 10:50:02 -0300

    tests: refatora os testes

--- tests/CalculadoraTestes.cs
+++ tests/CalculadoraTestes.cs
@@ -2,18 +2,21 @@ using src;
 
 namespace tests;
 
-public class CalculadoraTestes
+public class _calculadoraTestes
 {
+    private Calculadora _calc;
+    public _calculadoraTestes()
+    {
+        _calc = new Calculadora();
+    }
+
     [Theory]
     [InlineData(1, 2, 3)]
     [InlineData(3, 3, 6)]
     [InlineData(5, 5, 10)]
     public void SumTwoNumbersAndReturnResult(int n1, int n2, int result)
     {
-        Calculadora calc = new Calculadora();
-
-        int testResult = calc.Somar(n1, n2);
-
+        int testResult = _calc.Somar(n1, n2);
         Assert.Equal(result, testResult);
     }
 
@@ -23,10 +26,7 @@ public class CalculadoraTestes
     [InlineData(5, 5, 0)]
     public void SubtractTwoNumbersAndReturnResult(int n1, int n2, int result)
     {
-        Calculadora calc = new Calculadora();
-
-        int testResult = calc.Subtrair(n1, n2);
-
+        int testResult = _calc.Subtrair(n1, n2);
         Assert.Equal(result, testResult);
     }
 
@@ -36,10 +36,7 @@ public class CalculadoraTestes
     [InlineData(10, 2, 5)]
     public void DivideTwoNumbersAndReturnResult(int n1, int n2, int result)
     {
-        Calculadora calc = new Calculadora();
-
-        int testResult = calc.Dividir(n1, n2);
-
+        int testResult = _calc.Dividir(n1, n2);
         Assert.Equal(result, testResult);
     }
 
@@ -49,34 +46,28 @@ public class CalculadoraTestes
     [InlineData(5, 5, 25)]
     public void MultiplyTwoNumbersAndReturnResult(int n1, int n2, int result)
     {
-        Calculadora calc = new Calculadora();
-
-        int testResult = calc.Multiplicar(n1, n2);
-
+        int testResult = _calc.Multiplicar(n1, n2);
         Assert.Equal(result, testResult);
     }
 
     [Fact]
     public void DivideByZeroTest()
     {
-        Calculadora calc = new Calculadora();
-
         Assert.Throws<DivideByZeroException>(
-            () => calc.Dividir(3, 0)
+            () => _calc.Dividir(3, 0)
         );
     }
 
     [Fact]
     public void HistoryListTest()
     {
-        Calculadora calc = new Calculadora();
-        calc.Somar(1, 2);
-        calc.Somar(1, 2);
-        calc.Somar(1, 2);
+        _calc.Somar(1, 2);
+        _calc.Somar(1, 2);
+        _calc.Somar(1, 2);
 
-        var lista = calc.ListarHistorico(3);
+        var lista = _calc.ListarHistorico(3);
 
-        Assert.NotEmpty(calc.ListarHistorico(3));
+        Assert.NotEmpty(_calc.ListarHistorico(3));
         Assert.Equal(3, lista.Count);
     }
 }
