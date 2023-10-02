c4aed52317e96d8efaf171e08f7929489069dbff
dizzysch <victorwilbert@gmail.com>
2023-10-02 09:55:34 -0300

    feat: add estrutura inicial

---
+++ src/Calculadora.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class Calculadora
+    {
+        public int somar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int subtrair(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int multiplicar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int dividir(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public List<string> historico()
+        {
+            return new List<string>();
+        }
+    }
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using src;
+
+namespace tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Calculadora calc = new Calculadora();
+    }
+}
